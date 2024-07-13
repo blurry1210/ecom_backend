b2b834d241e90b03915d55dacb3824ddfd9c518d
Borza Alex-Cristian <netreach.ro@gmail.com>
2024-07-13 14:42:32 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
-const database = require('./config/database');
-const eventRoutes = require('./routes/eventRoutes');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
 
+const itemRoutes = require('./routes/itemRoutes');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const statsRoutes = require('./routes/statsRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+
+dotenv.config();
 const app = express();
-app.use(express.json());
-app.use('/events', eventRoutes);
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use(
+    cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/api/users', userRoutes);
+app.use('/api/items', itemRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/stats', statsRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/payments', paymentRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello from Express backend!');
 });
+app.post("/api/admin/login", (req, res) => {
+  const { password } = req.body;
+  if (password === process.env.ADMIN_PASSWORD) {
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ message: "Incorrect password" });
+  }
+});
+
+const dbURI = process.env.DB_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected…'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+console.log('Stripe Secret Key:', process.env.STRIPE_SECRET_KEY);
--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const eventController = require('../controllers/eventController');
+    const express = require('express');
+    const router = express.Router();
+    const eventController = require('../controllers/eventController');
 
-router.get('/', eventController.getEvents);
-router.post('/', eventController.createEvent);
+    router.get('/', eventController.getEvents);
+    router.post('/', eventController.createEvent);
 
-module.exports = router;
+    module.exports = router;
