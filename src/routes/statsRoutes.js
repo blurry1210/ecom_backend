156b782b9c5720ee36470a6e3da3d965c0ac9506
Alexandru-Marian Calin <alexandrucalin@proton.me>
2024-07-30 21:02:43 +0300

    microservices branch initial commit

--- src/routes/statsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDistributorStats, getDistributorOrderStatuses } = require('../controllers/statsController');
-const authenticate = require('../middleware/authenticate');
-
-router.get('/distributor/:distributorId', authenticate, getDistributorStats);
-router.get('/distributor/:distributorId/statuses', authenticate, getDistributorOrderStatuses);
-
-module.exports = router;
